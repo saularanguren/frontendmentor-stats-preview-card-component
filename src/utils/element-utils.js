59f4093af87dd0bf469dba9f9a4be4a88ceb7e7a
Saul Gabriel Aranguren <gabomoralejo@gmail.com>
2024-12-07 23:18:14 -0400

    first commit

---
+++ src/utils/element-utils.js
@@ -0,0 +1,18 @@
+export const increase_stats = (array_id, array_number, array_tag) => {
+    for(let i = 0; i < array_id.length; i++) {
+        let count = 0;
+        let time = null;
+
+        time = (array_number[i] > 100) ? 1 : 50;
+
+        const interval = setInterval(() => {
+            document.querySelector(array_id[i]).textContent = count;
+            count++;
+                
+            if(count > array_number[i]) {
+                document.querySelector(array_id[i]).textContent = (count-1) + array_tag[i];
+                clearInterval(interval)
+            }
+        }, time)            
+    }
+}
